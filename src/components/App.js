c86fcea0b7a93c911bc876a5d401ca7462b9df3e
Color6lind <zhigailov42@gmail.com>
2020-10-03 21:10:43 +0700

    Waves Keeper Auth integrated

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
 import React from 'react'
 
 function App() {
-
+    const authFunc = () => {
+        const authData = { data: "Auth on my site" };
+        if (WavesKeeper) {
+            WavesKeeper.auth( authData )
+                .then(auth => {
+                    console.log( auth );
+                }).catch(error => {
+                console.error( error );
+            })
+        } else {
+            alert("To Auth WavesKeeper should be installed.");
+        }
+    }
     return (
         <div className="container">
             <input className="btn btn-primary"
                    type="submit"
-                   value="Alert"
-                   onClick={() => {
-                       alert("Alert button onClick");
-                   }}
+                   value="Auth"
+                   onClick={authFunc}
             />
         </div>
     )
